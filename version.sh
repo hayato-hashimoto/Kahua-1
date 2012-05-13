49b6105ddcdf00f445afe8c5f3696ada6a5de9a8
Hayato Hashimoto (橋本 隼人) <hayato.hashimoto@gmail.com>
2012-05-13 23:31:46 +0900

    revised git version string

--- version.sh
+++ version.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-echo -n "1.1_pre "
-if expr "z`svnversion`" : z[0-9][0-9]*\$ > /dev/null
-then
-echo "svn-r`svnversion`"
-else
-git_svn_upstream=remotes/trunk
 if branch_name=`git symbolic-ref -q HEAD`
 then
  branch_name=`expr z\`git symbolic-ref HEAD\` : 'zrefs/heads/\(.*\)'`
 else
- branch_name=`** detached HEAD **`
+ branch_name='** detached HEAD **'
 fi
-echo "svn-r`git svn find-rev \`git merge-base HEAD $git_svn_upstream\``+git-`git rev-parse --short=8 HEAD` ($branch_name)"
-fi
+echo "git-`git log -1 --date=short --pretty=format:'%ad %h'` ($branch_name)"
